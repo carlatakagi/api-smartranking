eedcb230a06aeb08996bbefea63b5b81982223eb
carlatakagi <carlatakagi@gmail.com>
2023-01-22 20:34:32 -0400

    feat: changed files

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { PlayersModule } from './players/players.module';
 @Module({
   imports: [
     MongooseModule.forRoot(
-      'mongodb+srv://admin:wBOmY07q20QDOFzp@cluster0.sqg98ke.mongodb.net/?retryWrites=true&w=majority',
+      'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
       {
         useNewUrlParser: true,
         useCreateIndex: true,
